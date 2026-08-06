f4628e2aa226a2a9b8bd44889773e36b7dc30eb8
Randall Ridley <randall.ridley@gmail.com>
2026-08-06 08:50:51 -0500

    Update group-order.tsx

--- src/state/group-order.tsx
+++ src/state/group-order.tsx
@@ -74,29 +74,6 @@ export type GroupOrderContextValue = {
 
 const GroupOrderContext = createContext<GroupOrderContextValue | null>(null);
 
-const MENU_PRODUCTS: Product[] = [
-  {
-    id: "orange-dream-machine",
-    name: "Orange Dream Machine",
-    priceCents: 659,
-    imageUrl:
-      "https://go-dash.s3.us-west-2.amazonaws.com/jj-orange-dream-machine.webp",
-  },
-  {
-    id: "strawberry-surf-rider",
-    name: "Strawberry Surf Rider",
-    priceCents: 659,
-    imageUrl:
-      "https://go-dash.s3.us-west-2.amazonaws.com/jj-strawberry-surf.webp",
-  },
-  {
-    id: "acai-primo-bowl",
-    name: "Açaí Primo Bowl",
-    priceCents: 899,
-    imageUrl: "https://go-dash.s3.us-west-2.amazonaws.com/jj-acai.webp",
-  },
-];
-
 function normalizeEmail(value: string) {
   return value.trim().toLowerCase();
 }
@@ -115,7 +92,7 @@ export function GroupOrderProvider({
     cartsByEmail: {},
   });
 
-  const [products, setProducts] = useState<Product[]>(MENU_PRODUCTS);
+  const [products, setProducts] = useState<Product[]>([]);
 
   const applyBackendGroup = useCallback((group: BackendGroup) => {
     setState((prev) => {

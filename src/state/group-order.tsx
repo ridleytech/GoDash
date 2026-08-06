095557d769ee11a5f16f09ba371b957a44b0df83
Randall Ridley <randall.ridley@gmail.com>
2026-08-06 07:31:11 -0500

    update S3 links

--- src/state/group-order.tsx
+++ src/state/group-order.tsx
@@ -80,21 +80,20 @@ const MENU_PRODUCTS: Product[] = [
     name: "Orange Dream Machine",
     priceCents: 659,
     imageUrl:
-      "https://www.jamba.com/_next/image?url=https%3A%2F%2Fctfassets.gotofoods.com%2Fzqt8tllj2cy0%2F6ni1SBAVpcxIzlMLescuio%2Fa15d0bb83ee1e47848daac06e3d8213d%2F746x600_Orange_Dream_Machine_OH.png&w=1920&q=75",
+      "https://go-dash.s3.us-west-2.amazonaws.com/jj-orange-dream-machine.webp",
   },
   {
     id: "strawberry-surf-rider",
     name: "Strawberry Surf Rider",
     priceCents: 659,
     imageUrl:
-      "https://www.jamba.com/_next/image?url=https%3A%2F%2Fctfassets.gotofoods.com%2Fzqt8tllj2cy0%2F6ZYI7KsKiCfyUar5GAFJU5%2Fcf4723f8910812328d8154b3510ee387%2F746x600_Strawberry_Surf_Rider_OH.png&w=1920&q=75",
+      "https://go-dash.s3.us-west-2.amazonaws.com/jj-strawberry-surf.webp",
   },
   {
     id: "acai-primo-bowl",
     name: "Açaí Primo Bowl",
     priceCents: 899,
-    imageUrl:
-      "https://www.jamba.com/_next/image?url=https%3A%2F%2Fctfassets.gotofoods.com%2Fzqt8tllj2cy0%2F1mrn6H4sPm4CKBhltkPuaR%2Fa3aa56e80a1cf506119e726c3045337f%2FJJ_1454718_746x600_LEVEL_Acai_Primo_Both_Sizes.png&w=1920&q=75",
+    imageUrl: "https://go-dash.s3.us-west-2.amazonaws.com/jj-acai.webp",
   },
 ];
 
